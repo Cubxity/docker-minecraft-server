1d756e35121615ea618ef55e69f4691703584669
Cubxity <contact@cubxity.dev>
2021-07-08 20:23:20 +0200

    feat: updated Tuinity

--- tuinity/build.sh
+++ tuinity/build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 declare -A runtimes
-runtimes[adopt8]="adoptopenjdk/openjdk8:jre8u292-b10-alpine"
-runtimes[adopt11]="adoptopenjdk/openjdk11:jre-11.0.10_9-alpine"
 runtimes[adopt16]="adoptopenjdk/openjdk16:jre-16.0.1_9-alpine"
 
-manifest=$(curl -s https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastSuccessfulBuild/api/json)
+version=1.17
+
+manifest=$(curl -s https://ci.codemc.io/job/Spottedleaf/job/Tuinity-$version/lastSuccessfulBuild/api/json)
 build=$(echo "$manifest" | jq -r ".number")
 
 for runtimeName in "${!runtimes[@]}"; do
-  echo "[tuinity] building tuinity-$build-$runtimeName"
+  echo "[tuinity] building tuinity-$version-$build-$runtimeName"
 
   docker build \
     --build-arg "RUNTIME=${runtimes[$runtimeName]}" \
     --build-arg "RUNTIME_NAME=$runtimeName" \
+    --build-arg "TUINITY_VERSION=$version" \
     --build-arg "TUINITY_BUILD=$build" \
-    --tag "cubxity/minecraft-server:tuinity-$build-$runtimeName" \
-    --tag "cubxity/minecraft-server:tuinity-$runtimeName" \
+    --tag "cubxity/minecraft-server:tuinity-$version-$build-$runtimeName" \
+    --tag "cubxity/minecraft-server:tuinity-$version-$runtimeName" \
     -f tuinity/Dockerfile \
     .
 done

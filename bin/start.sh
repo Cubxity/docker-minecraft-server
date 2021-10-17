939f9e44b8b1a1755fd25f3f1906ff0665494a29
Cubxity <contact@cubxity.dev>
2021-10-17 17:16:03 +0200

    build: trigger build

--- bin/start.sh
+++ bin/start.sh
@@ -31,7 +31,7 @@ MAX_MEMORY=${MAX_MEMORY:-${MEMORY}}
 
 JVM_XX_OPTS=${JVM_XX_OPTS:-"-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"}
 JVM_OPTS="-Xms$INIT_MEMORY -Xmx$MAX_MEMORY"
-D_OPTS=${D_OPTS:-"-DPaper.IgnoreJavaVersion=true -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"}
+D_OPTS=${D_OPTS:-"-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -DPaper.IgnoreJavaVersion=true"}
 
 if isTrue "$EULA"; then
   D_OPTS="$D_OPTS -Dcom.mojang.eula.agree=true"
